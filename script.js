4cab51170db1b8defafeb690293217c2f88364f1
Ismail2hussein <ihusseinmohamed629@gmail.com>
2023-06-14 20:53:08 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-// Good Luck 💪🏾
+function pluralize(words) {
+  let wordCount = {};
+  for (let i = 0; i < words.length; i++) {
+    if (wordCount[words[i]]) {
+      wordCount[words[i]]++;
+    } else {
+      wordCount[words[i]] = 1;
+    }
+  }
+  let pluralWords = [];
+  for (let word in wordCount) {
+    if (wordCount[word] > 1) {
+      pluralWords.push(word + "s");
+    }
+  }
+  return pluralWords;
+}
+console.log(pluralize(["cat","dog","cat","mouse"])); // Output: ["cats","dog","mouse"]
+console.log(pluralize(["car", "car", "car"])); // Output: ["cars"]
